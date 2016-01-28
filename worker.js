d34d87f560d955337aa1cc44fbc7aa371f9d2c2b
kostuyn <kostuyn@gmail.com>
2016-01-28 20:08:40 +0300

    commit

---
+++ worker.js
@@ -0,0 +1,30 @@
+var debug = require('debug')('clickberry:STUB:worker');
+var mongoose = require('mongoose');
+var moment = require('moment');
+var config = require('clickberry-config');
+var Bus = require('./lib/bus-service');
+var Entity = require('./models/entity');
+
+var bus = new Bus({addresses: config.get('nsqlookupd:addresses').split(',')});
+
+var options = {
+    server: {
+        socketOptions: {
+            keepAlive: 1,
+            connectTimeoutMS: 30000
+        }
+    },
+    replset: {
+        socketOptions: {
+            keepAlive: 1,
+            connectTimeoutMS: 30000
+        }
+    }
+};
+mongoose.connect(config.get('mongodb:connection'), options);
+
+bus.on('EVENT-NAME', function (e) {
+
+});
+
+debug('Listening for messages...');
